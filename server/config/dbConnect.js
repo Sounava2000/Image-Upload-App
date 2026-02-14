7fd9090124d00d1e59c294a73e302e8c4a07a295
Sounava Mukherjee <soumo2640@gmail.com>
2026-02-14 22:29:30 +0530

    Fixed db config

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv'
 export async function dbConnect() {
+  dotenv.config()
+
   try {
     const dbConnection = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/myapp"
-    );
+      process.env.MONGO_URL
+    ); 
+     console.log(process.env.MONGO_URL)
     console.log(`MongoDB Connected Successfully`);
   } catch (error) {
     console.log(error.message);
